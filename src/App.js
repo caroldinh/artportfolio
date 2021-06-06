1d8ca83a39efa10c14236d68c62fba923d9caf03
cyborg48 <cyborg414781@gmail.com>
2021-06-06 12:04:19 -0400

    update styles

--- src/App.js
+++ src/App.js
@@ -19,15 +19,19 @@ class App extends React.Component{
         <Router>
         <div className="sidebar">
 
-          <Link to="/"><h1>Caroline Dinh</h1></Link>
-          <p>mediums: acrylic / watercolor / digital</p>
-          <p>tools: krita, heavypaint, huion</p>
+        <Link to="/"><img src="images/pfp.jpg" class="profile"></img></Link>
+
+          <Link to="/"><h1>CDINHART</h1></Link>
+          
           
             <Link className="navlink" to="/painting">Painting</Link>
             <Link className="navlink" to="/digital">Digital</Link>
             <Link className="navlink" to="/graphic-design">Graphic Design</Link>
             <Link className="navlink" to="/media">Digital Media</Link>
-            <Link className="navlink" to="/commissions">Commission Me</Link>
+            <Link className="navlink" to="/writing">Writing</Link>
+            <a href="https://cdinhcomms.carrd.co/" target="_blank" className="navlink">Commissions</a>
+
+            <div className="social" id="instagram"></div>
                     
         </div>
 
@@ -54,8 +58,13 @@ class App extends React.Component{
             <h1>Commission me</h1>
             <Commissions />
           </Route>
+          <Route path="/writing">
+            <h1>Writing</h1>
+            <Writing />
+          </Route>
           <Route path="/">
-            <Best/>
+            <Home/>
+            
           </Route>
         </Switch>
         </div>
@@ -71,33 +80,14 @@ class App extends React.Component{
   }
 }
 
-function Best(){
+function Home(){
   return(
-  <div key="best">
-    <Image source="images/swing.jpg" title="Swing"
-          description="Painted in Krita and featured in Paper Crane Journal's inaugural issue 
-          <a href='https://en.calameo.com/books/006629503d10b805aa97b' target='_blank'>here.</a>">
-      </Image>
-      <Image source="images/html1.jpg"
-        description="Logo for Reinvented Magazine's Invent with Reinvented HTML workshop."></Image>
-     <Image source="images/conditions.JPG" title="Pre-Existing Conditions"
-      description="Acrylic on canvas (18 x 24 in.). Forthcoming in the Montpelier Artists on the Rise juried exhibition (2021)."></Image>
-    <Image source="images/electricityandwater.jpg" title="Electricity & Water Don't Mix"
-      description="Painted with watercolor with touches of ink and oil pastel. This piece was submitted as part of my AP 
-      Studio Art: 2D Design portfolio and appeared in the first issue of The Agapanthus Collective <a href='https://www.agapanthuscollective.com/biannual-issue-1/' 
-      target='_blank'>here.</a>"></Image>
-      <Image source="images/machinelearning.jpg"
-        description="Logo for Reinvented Magazine's Intro to Machine Learning workshop."></Image>
-        <Image source="images/chien_shiung_wu.jpeg" title="Chien-Shiung Wu"
-      description="Watercolor on watercolor paper (17 x 11 in.). This piece depicts Chinese American nuclear physicist Chien-Shiung Wu."></Image>
-      <Image source="images/picnic.jpg"
-        description="Icon for an art critique app."></Image>
-        <Image source="images/concert_mistress.JPG" title="Concert Mistress"
-      description="Oil on canvas board (12 x 15 in.). This piece was featured on the cover of The Wondrous Real magazine's <a href='https://www.wondrousrealmag.com/issue-2'
-      target='_blank'>second issue.</a>"></Image>
-    <Image source="images/careergirls.JPG" title="Career Girls"
-      description="Acrylic on canvas (24 x 18 in.). This piece was featured on the cover of Southchild Lit's first issue, <a href='https://southchildlit.wixsite.com/website-2'
-      target='_blank'>GLITCH IN THE SYSTEM.</a>"></Image>
+  <div>
+        <p>Hi there! Call me C (she/they). I make art.</p>
+        <p>I'm chiefly a <Link className="pink" to="/digital">digital artist,</Link> though I have some experience with <Link className="pink" to="/painting">painting</Link> and <Link className="pink" to="/graphic-design">graphic design.</Link></p>
+        <p>Digital art and graphic design <a href="https://cdinhcomms.carrd.co" target="_blank" className="pink">commissions</a> are currently open.</p>
+        <p>I also <Link className="blue" to="/writing">write.</Link> You can find some of my poetry and prose online.</p>
+        <p>Find me on <a href="https://instagram.com/cdinhart" target="_blank">Instagram</a> and <a href="https://twitter.com/cdinhart" target="_blank">Twitter</a> @cdinhart.</p>
   </div>
   );
 }
@@ -105,9 +95,13 @@ function Best(){
 function Painting(){
   return(
     <div key="painting">
-    <Image source="images/conditions.JPG" title="Pre-Existing Conditions"
-      description="Acrylic on canvas (18 x 24 in.). Forthcoming in the Montpelier Artists on the Rise juried exhibition (2021)."></Image>
-    <Image source="images/electricityandwater.jpg" title="Electricity & Water Don't Mix"
+    <Image source="images/conditions.JPG" 
+      title="Pre-Existing Conditions"
+      description="Acrylic on canvas (18 x 24 in.). Featured in the Montpelier Artists on the Rise juried exhibition (2021)."
+      alt="A girl with brown skin and dark brown hair stands inside a city bus, with signs reading 'MAKS REQUIRED' and '6 FEET APART' taped to the windows. She wears a 
+      lime green down coat, a sky blue scarf, and brown pants with a brown messenger's bag over one shoulder. She grips a the bus railing in her left hand. Her mask 
+      hangs around her face from one ear because in her left hand, she holds a blue Alburterol inhaler for asthma."></Image>
+     <Image source="images/electricityandwater.jpg" title="Electricity & Water Don't Mix"
       description="Painted with watercolor with touches of ink and oil pastel. This piece was submitted as part of my AP 
       Studio Art: 2D Design portfolio and appeared in the first issue of The Agapanthus Collective <a href='https://www.agapanthuscollective.com/biannual-issue-1/' 
       target='_blank'>here.</a>"></Image>
@@ -131,10 +125,12 @@ function Painting(){
 function DigitalArt(){
   return (
     <div key="digitalart">
-      <Image source="images/swing.jpg" title="Swing"
-          description="Painted in Krita and featured in Paper Crane Journal's inaugural issue 
-          <a href='https://en.calameo.com/books/006629503d10b805aa97b' target='_blank'>here.</a>">
-      </Image>
+      <Image source="images/swing.jpg" 
+      title="Swing"
+      description="Painted in Krita and featured in Paper Crane Journal's inaugural issue 
+          <a href='https://en.calameo.com/books/006629503d10b805aa97b' target='_blank'>here.</a>"
+      alt="Digitally-rendered painting. A young woman with tanned skin, dark brown hair, a green t-shirt, and maroon shorts stands on the seat of a swing, laughing to the side.
+        In the background, her younger self stands trepidatiously in front of the same swing."/>
       <Image source="images/roadtrip.jpg" title="Road Trip with Dreamers"
         description="Painted in Krita featuring characters from my old novel work-in-progress and submitted as part of my 
         AP Studio Art: 2D Design portfolio."></Image>
@@ -144,6 +140,11 @@ function DigitalArt(){
       description="Comic about the American Dream. Featured in <a href='https://www.ogmamagazine.com/' target='_blank'>ogma magazine</a> issue 07: culture.">
         <img className="expanded" src="images/disillusionment2.jpg"></img>
       </Image>
+      <Image source="images/no1.jpg" title="ROBOBITCH">
+        <img className="expanded" src="images/no2.jpg"></img>
+      </Image>
+      <Image source="images/frankenstein.jpg" title="FRANKENSTIEN: A New Musical Poster"
+        description="Mock poster design for Frankenstein: A New Musical, featuring William Frankenstein and The Creature."></Image>
     </div>
   );
 }
@@ -151,22 +152,24 @@ function DigitalArt(){
 function GraphicDesign(){
   return(
     <div key="design">
-       <Image source="images/votelulu.jpg"
-        description="Flyer for a student election campaign."></Image>
       <Image source="images/reinventathon.jpg"
         description="Logo for Reinvented Magazine's Reinvent-a-thon."></Image>
       <Image source="images/html1.jpg"
-        description="Logo for Reinvented Magazine's Invent with Reinvented HTML workshop."></Image>
-      <Image source="images/machinelearning.jpg"
-        description="Logo for Reinvented Magazine's Intro to Machine Learning workshop."></Image>
-      <Image source="images/picnic.jpg"
-        description="Icon for an art critique app."></Image>
+        description="Logo for Reinvented Magazine's Invent with Reinvented HTML workshop."
+        alt="Logo for Reinvented Magazine's HTML workshop. On the right, a browser window displays a wepbage with a pink and blue wave pattern and the text 
+        '<div class='html'>'. A box with the HTML tag symbol '</>' overlaps it. To the side are two warped net patterns."></Image>
+      <Image source="images/backslash.svg"
+      description="Logo for <a href='https://backslashlit.com/' target='_blank'>Backslash Lit</a>">
+        <img className="expanded" src="images/backslash_icon.svg"></img>
+      </Image>
       <Image source="images/rockets.jpg"
        description="Graphic for Reinvented Magazine."></Image>
        <Image source="images/open_source.jpg"
        description="Graphic for Reinvented Magazine."></Image>
        <Image source="images/computer.jpg"
        description="Graphic for Reinvented Magazine."></Image>
+       <Image source="images/umd_transparent.svg"
+       description="Entry for UMD '25 discord server logo contest."></Image>
     </div>
   );
 }
@@ -174,19 +177,14 @@ function GraphicDesign(){
 function Media(){
   return(
     <div key="media"> 
-      <Image source="https://media0.giphy.com/media/u4v0v5oQRNhPMosZte/giphy.gif"
-      description="Web-based livecoding. Created with <a href='https://hydra.ojack.xyz/' target='_blank'>Hydra.</a>"></Image>
+      
       <Image source="images/charonexpress.jpg"
       description="Interactive inspiration board for my novel built with HTML/CSS/JS. Visit <a href='https://charonexpress.web.app/' target='_blank'>here</a>."></Image>
       <Image source="images/blog.jpg"
       description="Personal blog slash shouting void slash coding exercise. Built with Jekyll and HTML/CSS/JS. Visit <a href='https://cyblog48.web.app/' target='_blank'>here</a>."></Image>
-      <Image source="https://media0.giphy.com/media/b3588R563XDZ85BAB4/giphy.gif"
-      description="p5.js sketch."></Image>
       <Image source="images/hatch.jpg" description="p5.js sketch."></Image>
       <Image source="images/titan.jpg"
       description="Web dev demo. Built with HTML/CSS."></Image>
-      <Image source="https://media3.giphy.com/media/UQ8xhtnDRwlRI8govz/giphy.gif"
-      description="Web-based livecoding. Created with <a href='https://hydra.ojack.xyz/' target='_blank'>Hydra.</a>"></Image>
       <Image source="images/thao.jpg"
       description="Thao & the Get Down Stay Down Lyric bot. Built using Python with Tweepy and Genius API. Visit <a href='https://twitter.com/thaolyricbot' target='_blank'>here</a>. Listen to Thao's music 
       <a href='https://open.spotify.com/artist/2dn0CGCWvtrwZ66bOeLNb2?si=UtU8lPTDRVay0TDFx5r11Q' target='_blank'>here</a>."></Image>
@@ -194,17 +192,53 @@ function Media(){
   );
 }
 
+function Writing(){
+  return(
+      <table width="100%">
+        <tr>
+          <th width="20%"><p class="pink">Prose</p></th>
+          <th width="80%">
+            <Pub link="https://www.ampleremains.com/01carolinedinh.html" title="Doppelgänger" press="Ample Remains"></Pub>
+            <Pub link="https://www.polluxjournal.com/issue-1/c-t-dinh" title="Parallel // Structure" press="Pollux Journal"></Pub>
+            <Pub link="https://flashpointsf.com/2021/04/09/deploy/" title="DEPLOY" press="Flash Point SF"></Pub>
+            <Pub link="https://www.interstellarlit.com/issue-02/machine-unlearning" title="MACHINE UNLEARNING" press="Interstellar Lit"></Pub>
+          </th>
+        </tr>
+        
+        <tr>
+          <th width="20%"><p class="pink">Poetry</p></th>
+            <th width="80%">
+              <Pub link="https://www.indigoliteraryjournal.com/caroline-dinh" title="Overlay / Interview with an Olympian" press="Indigo Literary Journal"></Pub>
+              <Pub link="http://strangehorizons.com/poetry/city-girls/" title="city girls" press="Strange Horizons"></Pub>
+              <Pub link="https://www.interstellarlit.com/issue-02/escapist-playlist" title="Escapist Playlist" press="Interstellar Lit"></Pub>
+              <Pub link="https://issuu.com/paracosmliteraryjournal/docs/issue_01_metamorphosis__4_" title="3 superhero poems" press="Paracosm Lit"></Pub>
+            </th>
+        </tr>
+      </table>
+    )
+}
+
+class Pub extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+  return(
+        <p><a href={this.props.link} target="_blank">{this.props.title}</a> - <i>{this.props.press}</i></p>
+    )
+  }
+}
+
 function Commissions(){
   return(
     <div key="commissions">
-      <p>I am currently not taking traditional art commissions.</p>
       <p>Digital art and graphic design commissions are currently <strong>open</strong>.</p>
-      <p>Pricing for graphic designs is $15 for simple designs of up to 4 colors (+$2 per additional color).</p>
+      <p>Pricing for graphic designs starts at $15. (May be more for complex designs.)</p>
       <p>Pricing for digital art will vary depending on the request. For reference, here is the pricing for a single character:</p>
       <p><ul>
-        <li><strong>Bust (shoulders up):</strong>$5 for lineart / $10 for flat color / $15 for full render</li>
-        <li><strong>Half-body (waist up):</strong>$8 for lineart / $15 for flat color / $22 for full render</li>
-        <li><strong>Full-body:</strong>$10 for lineart / $20 for flat color / $30 for full render</li>
+        <li><strong>Bust (shoulders up):</strong> $5 for lineart / $10 for flat color / $15 for full render</li>
+        <li><strong>Half-body (waist up):</strong> $8 for lineart / $15 for flat color / $22 for full render</li>
+        <li><strong>Full-body:</strong> $10 for lineart / $20 for flat color / $30 for full render</li>
       </ul></p>
       <p>I am willing to draw original characters, fan art, friends/family, pets, etc.</p>
       <p>NSFW and bigoted requests will be ignored.</p>
@@ -212,7 +246,13 @@ function Commissions(){
       <p>Pricing samples:</p>
       <img className="img-static" src="images/pricing.jpg"></img>
 
-      <p>Please fill in the following <a href="https://docs.google.com/forms/d/e/1FAIpQLSdSty2FrfdMvsH8_n-2TmTlhi562ousWMfxUl4Jtr3uPK6oxQ/viewform?usp=sf_link" target="_blank">form</a> to order a commission.</p>
+      <p>To order a commission, please reach out via:
+        <ul>
+          <li>Instagram: <a href="http://instagram.com/cdinhart" target="_blank">@cdinhart</a></li>
+          <li>Twitter: <a href="https://twitter.com/cdinhart" target="_blank">@cdinhart</a></li>
+          <li><a href="https://docs.google.com/forms/d/e/1FAIpQLSdSty2FrfdMvsH8_n-2TmTlhi562ousWMfxUl4Jtr3uPK6oxQ/viewform?usp=sf_link" target="_blank">Commission order form</a></li>
+        </ul>
+      </p>
 
       <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdSty2FrfdMvsH8_n-2TmTlhi562ousWMfxUl4Jtr3uPK6oxQ/viewform?embedded=true" width="100%" height="1000" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
     </div>
@@ -252,7 +292,8 @@ class Image extends React.Component{
       return (
         <div className={this.state.size} onClick={this.enlarge} ref={this.myRef}>
           <Suspense fallback={ <img className="cover fadeIn" src=''></img>}>
-             <img className="cover" src={this.props.source} loading="lazy"></img>
+             <img className="cover" src={this.props.source} 
+             alt={this.props.alt} loading="lazy"></img>
           </Suspense>
         </div>
       );
@@ -260,7 +301,7 @@ class Image extends React.Component{
       return (
           <div className={this.state.size} onClick={this.handleClick}>
             <a id="currentImage"></a>
-            <img className="expanded" src={this.props.source} loading="lazy"></img>
+            <img className="expanded" src={this.props.source} loading="lazy" alt={this.props.alt}></img>
             {this.props.children}
             <p className="img-title">{this.props.title}</p>
             <p className="img-description" dangerouslySetInnerHTML={this.setDescription()}></p>
@@ -278,29 +319,41 @@ class Footer extends React.Component{
   constructor(props){
     super(props);
     this.changeTheme = this.changeTheme.bind(this);
-    this.setState({dark: true});
+    this.state = {
+      dark: true,
+      icon: "images/sun.svg",
+      iconclass: "sun",
+    };
   }
 
   changeTheme(){
     if(document.documentElement.getAttribute("data-theme") === "dark"){
       document.documentElement.setAttribute("data-theme", "light");
-      this.setState({dark: false});
+      this.setState({
+        dark: false,
+        icon: "images/moon.svg",
+        iconclass: "moon",
+      });
     }
     else{
       document.documentElement.setAttribute("data-theme", "dark");
-      this.setState({dark: true});
+      this.setState({
+        dark: true,
+        icon: "images/sun.svg",
+        iconclass: "sun",
+      });
     }
   }
 
   render(){
     return(
         <div className="footer">
+
           <p className="footertext">site built with react.js & deployed with firebase hosting.</p>
           <Carbonbadge className="badge" darkMode={document.documentElement.getAttribute("data-theme") === "dark"} />
-          <p className="footertext">Change theme</p>
-              <label className="switch" onChange={this.changeTheme}>
+          <label className="switch" onChange={this.changeTheme}>
                 <input type="checkbox"/>
-                <span className="slider round"></span>
+                <span className="slider round"><img class={this.state.iconclass} src={this.state.icon}></img></span>
               </label>
         </div>
     );
